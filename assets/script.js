35bafe898f9314b7945d20a925fd4d5e44e4ff1b
SOG <solennguillonan@gmail.com>
2023-06-05 16:21:25 +0200

    finalisation projet

--- assets/script.js
+++ assets/script.js
@@ -21,18 +21,61 @@ const slides = [
 let cpt = 0;
 
 const banner_img = document.querySelector(".banner-img");
+const banner_tagline = document.querySelector(".banner-tagline");
+const tagLine = document.querySelector('.tagLine');
+const bullets = document.querySelector('.dots')
+
+
+const displayBullets = ()=>{
+	bullets.innerHTML = "";
+	for(let i = 0; slides.length > i; i++){
+		const bullet = document.createElement('div');
+		bullet.classList.add("dot")
+		if(cpt == i)
+			bullet.classList.add("dot_selected")
+
+		bullets.appendChild(bullet);		
+		}
+
+}
+
+
+const init = () =>{
+	tagLine.innerHTML = slides[cpt].tagLine;
+	displayBullets();
+	
+}
+
+
+
+init();
+
+
 
 
 const arrow_left = document.querySelector(".arrow_left");
 arrow_left.addEventListener("click",()=>{
-	// faire -1
-	cpt = cpt - 1;
+	if(cpt == 0){
+		cpt = slides.length-1;
+	}else{
+		cpt = cpt - 1;
+	}
+	
 	banner_img.src = "./assets/images/slideshow/"+slides[cpt].image
+	tagLine.innerHTML = slides[cpt].tagLine;
+	displayBullets();
 })
 
 
 const arrow_right = document.querySelector(".arrow_right");
-arrow_right.addEventListener("click",()=>{
-	cpt = cpt +1;
-	banner_img.src = "./assets/images/slideshow/"+slides[cpt].image
+arrow_right.addEventListener("click",()=>{	
+	if((slides.length-1) == cpt){
+		cpt =0;
+	}else{
+		cpt = cpt +1;
+	}
+	banner_img.src = "./assets/images/slideshow/"+slides[cpt].image;
+	tagLine.innerHTML = slides[cpt].tagLine;
+	displayBullets();
+	
 })
